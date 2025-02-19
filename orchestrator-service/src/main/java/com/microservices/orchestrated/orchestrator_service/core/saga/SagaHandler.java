7b9669e5665d378770d8bcb9bd17844ba65c5dde
Gabrielle_Rodrigues <gabrielle.rodrigues@dell.com>
2025-02-19 17:25:42 -0300

    ✨ Orchestrator

---
+++ orchestrator-service/src/main/java/com/microservices/orchestrated/orchestrator_service/core/saga/SagaHandler.java
@@ -0,0 +1,4 @@
+package com.microservices.orchestrated.orchestrator_service.core.saga;
+
+public class SagaHandler {
+}
---
+++ orchestrator-service/src/main/java/com/microservices/orchestrated/orchestrator_service/core/service/OrchestratorService.java
@@ -0,0 +1,4 @@
+package com.microservices.orchestrated.orchestrator_service.core.service;
+
+public class OrchestratorService {
+}
